0bdedbdbac07f97d3d02d41102364cc9aee8a25e
Jordi Galobart Salvat <jgalobart@gmail.com>
2024-06-26 19:34:04 +0200

    primer paso de websockets

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,33 @@ const server = http.createServer(app);
 
 app.use(cors());
 
+const { Server } = require("socket.io");
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  //detección de conexión
+  console.log("a user connected");
+  // detección de desconexión
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  //detección de nuevo evento
+  socket.on("login", (user) => {
+    console.log(user);
+    socket.emit("message", "Hola" + user.id);
+  });
+
+  socket.on("msg", (msg) => {
+    console.log("He recibido un nuevo mensaje", msg);
+    socket.emit("msg", msg);
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("<h1>Websockets!</h1>");
 });
